b37c1763834887a6c04f4f6c374785831026f03f
garcia2015 <garcia2015@fau.edu>
2018-12-24 00:19:58 -0500

    Update qDetect_v1.py

--- qDetect_v1.py
+++ qDetect_v1.py
@@ -4,93 +4,162 @@
 
 # import statements
 from pycorenlp import StanfordCoreNLP
-from nltk.tree import Tree
-import string
-#from nltk.parse.stanford import StanfordDependencyParser
+from nltk.tokenize import word_tokenize
+import spacy
+from spacy.tokens import Doc
+from spacy.tokens import Token
+import re
+import csv
 
 # needed to run StanfordCoreNLP server
 nlp = StanfordCoreNLP('http://localhost:9000')
 
+nlp2 = spacy.load('en_core_web_sm')
 
-def question_classifier(filePathIn, filePathOut):
+filePathIn1 = 'example2.txt'  # use example file for debugging, then training text files when classifier ready
+filePathOut1 = 'annotatedEx2.txt'
+filePathIn2 = filePathOut1
+filePathOut2 = 'annotatedEx2Flat.txt'
 ########################################################################################################################
 #
-# This block of text opens text file, reads each line, and parses each line using Stanford Parser. If the annotated
-# line contains clause label SBARQ or SQ, then the line is a question. If it is a question, a 1 is printed to its cor-
-# responding line in a new file; if it is NOT a question, a 0 will print to its corresponding line in the file.
+# This block of text opens text file, reads and turns each line into annotated Tree and writes it to new text file,
+#  with a \r\n at the end of each Tree to help in next step
 #
-#  4 Question types included in this classifier are as follows:
-#
-#  SBARQ clause label indicates:
-#       (1) WH-Questions (which contain an SQ clause as well)
-#  SQ denotes
-#       (2) Yes/No questions,
-#       (3) Subject-less yes/no questions,
-#       (4) Tag questions, and
-#       (5) choice questions
-#           (which basically are yes/no questions since first part is auxiliary verb followed by "or" and second choice)
-#
-#  Therefore, questions MUST contain SBARQ clauses, SQ clauses, or both clauses
-#  Any line of text that does NOT contain the above is NOT a question
-#  (indirect questions do not count as questions for the purpose of this classifier)
-#
-#   Cases not covered:
-#   (1) indirect questions,
-#   (2) questions that read like statements when no question mark present (e.g. "This is Japan" vs "This is Japan?")
-#   (3)
-#
-# (Reference: "Bracketing Guidelines for Treebank II Style Penn Treebank Project", sections 1.2.5 - 1.2.6
-# (URL: http://www.sfs.uni-tuebingen.de/~dm/07/autumn/795.10/ptb-annotation-guide/root.html)
-# See also: https://preply.com/en/blog/2014/11/13/types-of-questions-in-english/
+# Data preprocessing/cleaning is done outside of this code since assignment prompt indicated we are to assume the input
+# string to have no end-punctuation
 #
 #######################################################################################################################
-    with open(filePathIn) as fp:  # open text file and read line by line
-        counter = 1
-        print("Reading file 1:")
-        translator = str.maketrans('', '', string.punctuation)          #   remove punctuation
-        line = (fp.readline()).translate(translator)                    #   ensure line is only read once
-        classifiedText = open(filePathOut, 'a')
-        # print labels on first line of classifiedText file
-        classifiedText.write("SBARQ\tSQ\tCLASS\n")
-        while line:
-            print("Reading Line {}: {}".format(counter, line.strip()))
-            outputRaw = nlp.annotate(line, properties={
-                'annotators': 'tokenize,ssplit,pos,depparse,parse',
-                'outputFormat': 'json'
-            })
-            parseTree = outputRaw['sentences'][0]['parse']
-            label1Val = "0\t"  # SBARQ label
-            label2Val = "0\t"  # SQ label
-            label3Val = "0\t"  # Class label
+with open(filePathIn1) as fp:  # open text file and read line by line
+    counter = 0
+    print("Reading file 1:")
+    line = fp.readline()
+    # print("Line {}: {}".format(counter, line.strip()))
+    while line:
+        print("Reading Line {}: {}".format(counter, line.strip()))
+        # ptree = ParentedTree.fromstring(format(counter, str(nlp.parse(line.split()))))    # used for testing
+        # resultTree = nlp.parse(line.split())                                               # used for testing
+        outputRaw = nlp.annotate(line, properties={
+            'annotators': 'tokenize,ssplit,pos,depparse,parse',
+            'outputFormat': 'json'
+        })
+        # print(outputRaw['sentences'][0]['parse'])                                              # used for testing
 
-            #print(parseTree) #used to test
+        annotatedEx2 = open(filePathOut1, 'a')
+        annotatedEx2.write(outputRaw['sentences'][0]['parse'])
+        annotatedEx2.write("\n")
+        counter += 1
+        print("Added 1 to counter")
+        line = fp.readline()
+    annotatedEx2.close()
+fp.close()
 
-            for i in Tree.fromstring(parseTree).subtrees():
-                if i.label() == 'SBARQ':
-                    print(i.leaves(), i.label())  # used for testing
-                    label1Val = "1\t"
-                    label3Val = "1\t"
-                    print("SBARQ")
-                elif i.label() == 'SQ':
-                    print(i.leaves(), i.label())  # used for testing
-                    label2Val = "1\t"
-                    label3Val = "1\t"
-                    print("SQ")
+########################################################################################################################
+#
+# This block of code turns tree into 1 line, tokenized, with parentheses removed
+# then we search each line for SBAR, SBARQ, SINV + NEG, SINV + Or
+# See next comment block for details on 4 types of questions
+#
+########################################################################################################################
+
+with open(filePathIn2) as fp2:
+    counter = 0
+    print("Reading file 2:")
+    line = fp2.readline()
+    while line:
+        annotatedEx2Flat = open(filePathOut2, 'a')
+        print("This is line without brackets ")
+        bracketsRemoved = line.strip("()[]")
+        print(bracketsRemoved)
+        doc = nlp2(bracketsRemoved)
+        docStatement = Doc(doc.vocab, words=[t.text for t in doc])
+        blockStr = ""
+        print("This is first block of line")
+        print(blockStr)
+        tokens_text = [t.text for t in doc]
+        while tokens_text:
+            tagWord = tokens_text
+            print("This is tagWord")
+            print(tagWord)
+            blockStr = blockStr + tagWord
+            print("Block")
+            print(blockStr)
+            print("This is block after appending")
+            print(blockStr)
+            if tagWord == "SBAR" or token.text == "SBARQ":      # this covers question types 1 and 2
+                questionLabel = '1\n'
+                print("Tagword is SBAR or SBARQ and questionLabel is " + questionLabel)
+            elif tagWord == "SINV":
+                print("Tagword is SINV")                        # this covers question types 3 and 4
+                if (t.text for t in doc) == "NEG":
+                    questionLabel = '1\n'
+                    print("SINV followed by NEG and questionLabel is " + questionLabel)
+                elif (t.text for t in doc) == "or":
+                    questionLabel = '1\n'
+                    print("SINV followed by Or and questionLabel is " + questionLabel)
                 else:
-                    print(" ")
-            classifiedText.write(label1Val + label2Val + label3Val + "\n")
-            counter += 1
-            print("Added 1 to counter")
-            line = fp.readline()
-    print("No more lines")
-    classifiedText.close()
-    fp.close()
-    return
+                    questionLabel = '0\n'
+                    print("Not a type 3 or 4 question and questionLabel is " + questionLabel)        # used for testing
+            else:
+                questionLabel = '0\n'
+                print("Not a question and questionLabel is " + questionLabel)
+        else:
+            blockStr = blockStr + questionLabel
+            annotatedEx2Flat.write(blockStr)
+            print("Doc complete:\n " + blockStr)
+    else:
+        print("File complete")
+    print("No more lines in file left to read. Closing output file 2")
+    annotatedEx2Flat.close()
+print("Closing Input File 2")
+fp2.close()
+########################################################################################################################
+#
+# This block of code is meant to search through each line of the flattened tree for markers/tags which indicate
+# the statements are questions (see 4 types of questions below)
+#
+# The best way to do this is to use spacy to tokenize the flattened annotation text
+#
+# To search through for the 4 different tags which indicate the questions, set up if statements or cases that if tag
+# for question type 1, 2, 3, or 4 is found, return a 1
+# If none of the tags were found, return a 0, indicating text is not a question
+# The number of lines in this new file (referred to as results.txt should match the number of lines in original file)
+#
+#######################################################################################################################
+#   4 types of questions:
+#       (1) WH Questions                -- indicated by SBARQ tag produced by Stanford Parser
+#               ex. Questions which start with Who/What/When/Where/Why/How
+#
+#       (2) Yes/No Questions            -- indicated by SBAR tag
+#               ex. Is it raining in Phoenix?
+#
+#       (3) Choice Questions            -- indicated by SINV tag followed by CC and Or
+#               ex. Do you like the Atlanta Braves or Boston Red Sox better?
+#
+#       (4) Tag/disjunctive Questions   -- SINV and a NEG; edge case tested
+#               ex. It is really cold out, isn't i?
+#               edge case: Your father isn’t working today, is he?
+#
+########################################################################################################################
+#
+# This part of the code is meant to implement the classifier on the results.txt file
+# LibSVM ws found to have the best results
+#
+# from sklearn.svm import LinearSVC
+# import pandas
+#
+# en_doc = en_nlp(u'' + question_to_predict)
+# question_data = get_question_predict_features(en_doc)
+# X_predict = pandas.get_dummies(question_data)
+#
+# y = dta.pop('questionLabel')
+# dta.pop('blockStr')
+#
+# X_train = pandas.get_dummies(dta)   #get_dummies() function converts the actual values into dummy values or binary values
+
+#
+# additional code I was planning on modifying and implementing with StanfordCoreNLP
+# parser at https://shirishkadam.com/category/2017/
+######################################################################################################################
 
 
-#MAIN
-
-#question_classifier('trainingSents1.txt', 'classifiedSents1.txt')
-#question_classifier('trainingSents2.txt', 'classifiedSents2.txt')
-#question_classifier('trainingSents3.txt', 'classifiedSents3.txt')
-question_classifier('test-inputs.txt', 'test-outputs.txt')
+# End of code
